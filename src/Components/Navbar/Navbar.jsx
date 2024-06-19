276c8cb67f41c0e1243b98b38c0d1e1b7024e912
sai kiran <saikiran@sais-MacBook-Air.local>
2024-06-19 12:22:30 +0530

    done-nav-ui

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,13 @@
+import { Box } from '@chakra-ui/react'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+        <Box>
+
+        </Box>
+    </>
   )
 }
 
