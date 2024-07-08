9f74bbe7fc2efd484bfab335426f1c11dd56fd63
Praneesha <pranesha182004@gmail.com>
2024-07-08 17:43:57 +0530

    commit1

--- src/Pages/SecretOfSuccess/SecretOfSuccess.jsx
+++ src/Pages/SecretOfSuccess/SecretOfSuccess.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
+import React from "react";
+import { Box,Text } from "@chakra-ui/react";
+import image1 from "../../Assets/sosimage1.jpg";
 const SecretOfSuccess = () => {
   return (
-    <div>SecretOfSuccess</div>
-  )
-}
+    <>
+      <Box mt='150px'>
+        <Box width='60%' display='flex' alignItems='center' justifyContent='center' h='100px' bg='red'></Box>
+      </Box>
+    </>
+  );
+};
 
-export default SecretOfSuccess
+export default SecretOfSuccess;
