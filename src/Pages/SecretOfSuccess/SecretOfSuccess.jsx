dc4a106b9b3b670ca8371bb9c8cab77da916ed40
Praneesha <pranesha182004@gmail.com>
2024-07-08 18:10:41 +0530

    added commit2

--- src/Pages/SecretOfSuccess/SecretOfSuccess.jsx
+++ src/Pages/SecretOfSuccess/SecretOfSuccess.jsx
@@ -1,11 +1,105 @@
 import React from "react";
-import { Box,Text } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import image1 from "../../Assets/sosimage1.jpg";
 const SecretOfSuccess = () => {
   return (
     <>
-      <Box mt='150px'>
-        <Box width='60%' display='flex' alignItems='center' justifyContent='center' h='100px' bg='red'></Box>
+      <Box mt="150px">
+        <Box display="flex" justifyContent="center" alignItems="center">
+          <Box
+            w="60%"
+            h="800px"
+            bg="green"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            gap='50px'
+          >
+            <Box display='flex' flexDirection='column' justifyContent='center' alignItems='center' w="60%" gap='50px'>
+            <Box
+              w='100%'
+              h="150px"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              gap='20px'
+              padding='10px'
+              bg='blue'
+            >
+              <Box>CHARACTER</Box>
+              <Box>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+                quo id dolores culpa! Doloremque dolor maxime animi architecto
+                maiores reprehenderit pariatur non vero nostrum, incidunt
+                tempora consequatur odit inventore sunt possimus perferendis
+                porro.
+              </Box>
+            </Box>
+
+            <Box
+              w="100%"
+              h="150px"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              gap='20px'
+              padding='10px'
+              bg='blue'
+            >
+              <Box>CHARACTER</Box>
+              <Box>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+                quo id dolores culpa! Doloremque dolor maxime animi architecto
+                maiores reprehenderit pariatur non vero nostrum, incidunt
+                tempora consequatur odit inventore sunt possimus perferendis
+                porro.
+              </Box>
+            </Box>
+
+            <Box
+              w="100%"
+              h="150px"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              gap='20px'
+              padding='10px'
+              bg='blue'
+            >
+              <Box>CHARACTER</Box>
+              <Box>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+                quo id dolores culpa! Doloremque dolor maxime animi architecto
+                maiores reprehenderit pariatur non vero nostrum, incidunt
+                tempora consequatur odit inventore sunt possimus perferendis
+                porro.
+              </Box>
+            </Box>
+
+            <Box
+              w="100%"
+              h="150px"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              gap='20px'
+              padding='10px'
+              bg='blue'
+            >
+              <Box>CHARACTER</Box>
+              <Box>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+                quo id dolores culpa! Doloremque dolor maxime animi architecto
+                maiores reprehenderit pariatur non vero nostrum, incidunt
+                tempora consequatur odit inventore sunt possimus perferendis
+                porro.
+              </Box>
+            </Box>
+
+            </Box>
+            <Box h="800px" w="40%" bg='violet'></Box>
+          </Box>
+        </Box>
       </Box>
     </>
   );
