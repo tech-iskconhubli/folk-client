cd271be1db4008df00e15b173fcc5ff6c8b41cd2
Praneesha <pranesha182004@gmail.com>
2024-07-10 11:22:57 +0530

    updated secret of success

--- src/Pages/SecretOfSuccess/SecretOfSuccess.jsx
+++ src/Pages/SecretOfSuccess/SecretOfSuccess.jsx
@@ -1,103 +1,142 @@
 import React from "react";
 import { Box, Text } from "@chakra-ui/react";
 import image1 from "../../Assets/sosimage1.jpg";
+import image2 from "../../Assets/sosimage2.png";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef, useState } from "react";
 const SecretOfSuccess = () => {
+  const card1 = useRef();
+  const { contextSafe } = useGSAP({ scope: card1 });
+  const handlecard1 = contextSafe(() => {
+    gsap.to(card1.current, { x: 145, duration: 0.5 });
+  });
+  const handlecard1leave = contextSafe(() => {
+    gsap.to(card1.current, { x: 0, duration: 0.5 });
+  });
   return (
     <>
       <Box mt="150px">
-        <Box display="flex" justifyContent="center" alignItems="center">
+        <Box
+          boxSizing="border-box"
+          p="0"
+          m="0"
+          bg="#F0EAD6"
+          h="600px"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          mt="200px"
+        >
           <Box
             w="60%"
-            h="800px"
-            bg="green"
+            h="100%"
             display="flex"
             alignItems="center"
             justifyContent="center"
-            gap='50px'
           >
-            <Box display='flex' flexDirection='column' justifyContent='center' alignItems='center' w="60%" gap='50px'>
             <Box
-              w='100%'
-              h="150px"
+              w="70%"
+              h="600px"
               display="flex"
-              alignItems="center"
+              flexDirection="column"
               justifyContent="center"
-              gap='20px'
-              padding='10px'
-              bg='blue'
-            >
-              <Box>CHARACTER</Box>
-              <Box>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
-                quo id dolores culpa! Doloremque dolor maxime animi architecto
-                maiores reprehenderit pariatur non vero nostrum, incidunt
-                tempora consequatur odit inventore sunt possimus perferendis
-                porro.
-              </Box>
-            </Box>
-
-            <Box
-              w="100%"
-              h="150px"
-              display="flex"
               alignItems="center"
-              justifyContent="center"
-              gap='20px'
-              padding='10px'
-              bg='blue'
+              gap='15px'
             >
-              <Box>CHARACTER</Box>
-              <Box>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
-                quo id dolores culpa! Doloremque dolor maxime animi architecto
-                maiores reprehenderit pariatur non vero nostrum, incidunt
-                tempora consequatur odit inventore sunt possimus perferendis
-                porro.
+              <Box
+                w="90%"
+                h="140px"
+                p="20px"
+                display="flex"
+                alignItems="center"
+                justifyContent="space-between"
+                bg='#DE8D38'
+                textAlign='justify'
+                gap='30px'
+                borderRadius='5px'
+                
+              >
+                {/* <Box>heading</Box> */}
+                <Box>
+                  <Text>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Itaque dolorum quae at neque labore! Rem dolor modi fugiat
+                    nesciunt eum excepturi consequuntur facilis commodi
+                    veritatis facere laudantium eos at, incidunt labore pariatur
+                  </Text>
+                </Box>
               </Box>
-            </Box>
 
-            <Box
-              w="100%"
-              h="150px"
-              display="flex"
-              alignItems="center"
-              justifyContent="center"
-              gap='20px'
-              padding='10px'
-              bg='blue'
-            >
-              <Box>CHARACTER</Box>
-              <Box>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
-                quo id dolores culpa! Doloremque dolor maxime animi architecto
-                maiores reprehenderit pariatur non vero nostrum, incidunt
-                tempora consequatur odit inventore sunt possimus perferendis
-                porro.
+              <Box
+                w="90%"
+                h="140px"
+                p="20px"
+                display="flex"
+                alignItems="center"
+                justifyContent="space-between"
+                bg='#DE8D38'
+                textAlign='justify'
+                gap='30px'
+                borderRadius='5px'
+                
+              >
+                {/* <Box>heading</Box> */}
+                <Box>
+                  <Text>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Itaque dolorum quae at neque labore! Rem dolor modi fugiat
+                    nesciunt eum excepturi consequuntur facilis commodi
+                  </Text>
+                </Box>
               </Box>
-            </Box>
 
-            <Box
-              w="100%"
-              h="150px"
-              display="flex"
-              alignItems="center"
-              justifyContent="center"
-              gap='20px'
-              padding='10px'
-              bg='blue'
-            >
-              <Box>CHARACTER</Box>
-              <Box>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
-                quo id dolores culpa! Doloremque dolor maxime animi architecto
-                maiores reprehenderit pariatur non vero nostrum, incidunt
-                tempora consequatur odit inventore sunt possimus perferendis
-                porro.
+              <Box
+                w="90%"
+                h="140px"
+                p="20px"
+                display="flex"
+                alignItems="center"
+                justifyContent="space-between"
+                bg='#DE8D38'
+                textAlign='justify'
+                gap='30px'
+                borderRadius='5px'
+              >
+                {/* <Box>heading</Box> */}
+                <Box>
+                  <Text>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Itaque dolorum quae at neque labore! Rem dolor modi fugiat
+                    nesciunt eum excepturi consequuntur facilis commodi
+                  </Text>
+                </Box>
               </Box>
-            </Box>
+
+              <Box
+                w="90%"
+                h="140px"
+                p="20px"
+                display="flex"
+                alignItems="center"
+                justifyContent="space-between"
+                bg='#DE8D38'
+                textAlign='justify'
+                gap='30px'
+                borderRadius='5px'
+              >
+                {/* <Box>heading</Box> */}
+                <Box>
+                  <Text>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Itaque dolorum quae at neque labore! Rem dolor modi fugiat
+                    nesciunt eum excepturi consequuntur facilis commodi
+                  </Text>
+                </Box>
+              </Box>
 
             </Box>
-            <Box h="800px" w="40%" bg='violet'></Box>
+            <Box w="30%" h="600px" backgroundImage={image2} backgroundSize='cover'></Box>
           </Box>
         </Box>
       </Box>
