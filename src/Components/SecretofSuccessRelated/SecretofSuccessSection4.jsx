9d7a84ee0921d6dab82525e36adacaf64363e510
Praneesha <pranesha182004@gmail.com>
2024-07-20 17:07:43 +0530

    made sos responsive

--- src/Components/SecretofSuccessRelated/SecretofSuccessSection4.jsx
+++ src/Components/SecretofSuccessRelated/SecretofSuccessSection4.jsx
@@ -11,33 +11,98 @@ import gsap from "gsap";
 const SecretofSuccessSection4 = () => {
   const [hoveredText, setHoveredText] = useState("");
   const Animatedcircle = useRef([]);
-  const [textIndex,setTextIndex]=useState(null)
+  const [textIndex, setTextIndex] = useState(null);
   const animatedText = useRef([]);
 
   useGSAP(() => {
-    
-    gsap.fromTo(
-      Animatedcircle.current,
-      {
-        opacity: 1,
-        transform: (i) =>
-          `rotate(-${i * 60 + 90}deg) translate(100px) rotate(${
-            i * 60 + 90
-          }deg)`,
-      },
-      {
-        rotate: "360deg",
-        opacity: 1,
-        transform: (i) =>
-          `rotate(-${i * 60 + 90}deg) translate(225px) rotate(${
-            i * 60 + 90
-          }deg)`,
-        duration: 2,
-        ease: "power2.out",
-      }
-    );
+    const width = window.innerWidth;
+    gsap.killTweensOf(Animatedcircle.current);
+    if (width < 768) {
+      gsap.fromTo(
+        Animatedcircle.current,
+        {
+          opacity: 1,
+          transform: (i) =>
+            `rotate(-${i * 60 + 90}deg) translate(50px) rotate(${
+              i * 60 + 90
+            }deg)`,
+        },
+        {
+          rotate: "360deg",
+          opacity: 1,
+          transform: (i) =>
+            `rotate(-${i * 60 + 90}deg) translate(125px) rotate(${
+              i * 60 + 90
+            }deg)`,
+          duration: 2,
+          ease: "power2.out",
+        }
+      );
+    } else if (width >= 768 && width < 992) {
+      gsap.fromTo(
+        Animatedcircle.current,
+        {
+          opacity: 1,
+          transform: (i) =>
+            `rotate(-${i * 60 + 90}deg) translate(75px) rotate(${
+              i * 60 + 90
+            }deg)`,
+        },
+        {
+          rotate: "360deg",
+          opacity: 1,
+          transform: (i) =>
+            `rotate(-${i * 60 + 90}deg) translate(150px) rotate(${
+              i * 60 + 90
+            }deg)`,
+          duration: 2,
+          ease: "power2.out",
+        }
+      );
+    } else if (width >= 992 && width < 1280) {
+      gsap.fromTo(
+        Animatedcircle.current,
+        {
+          opacity: 1,
+          transform: (i) =>
+            `rotate(-${i * 60 + 90}deg) translate(90px) rotate(${
+              i * 60 + 90
+            }deg)`,
+        },
+        {
+          rotate: "360deg",
+          opacity: 1,
+          transform: (i) =>
+            `rotate(-${i * 60 + 90}deg) translate(200px) rotate(${
+              i * 60 + 90
+            }deg)`,
+          duration: 2,
+          ease: "power2.out",
+        }
+      );
+    } else {
+      gsap.fromTo(
+        Animatedcircle.current,
+        {
+          opacity: 1,
+          transform: (i) =>
+            `rotate(-${i * 60 + 90}deg) translate(100px) rotate(${
+              i * 60 + 90
+            }deg)`,
+        },
+        {
+          rotate: "360deg",
+          opacity: 1,
+          transform: (i) =>
+            `rotate(-${i * 60 + 90}deg) translate(225px) rotate(${
+              i * 60 + 90
+            }deg)`,
+          duration: 2,
+          ease: "power2.out",
+        }
+      );
+    }
   });
-
 
   const paths = [
     {
@@ -65,32 +130,41 @@ const SecretofSuccessSection4 = () => {
       text: "Discipline is the foundation upon which success is built.",
     },
   ];
-  const handleMouseEnter = (text,index) => {
+  const handleMouseEnter = (text, index) => {
     setHoveredText(text);
     setTextIndex(index);
   };
 
   return (
     <>
-      <Box mt="50px" mb="50px">
-        <Box mb="50px" textAlign="center" fontSize={{base:'1.5rem',md:'2rem',lg:'2.5rem'}}>
-          Path to success
+      <Box>
+        <Box
+          mb="50px"
+          textAlign="center"
+          fontSize={{ base: "1.5rem", md: "2rem", lg: "2.5rem" }}
+        >
+          Path to Success
         </Box>
-        <Box display="flex" flexDirection={{base:'column',md:'row',lg:'row',xl:'row'}}alignItems="center" justifyContent="center">
+        <Box
+          display="flex"
+          flexDirection={{ base: "column", md: "row" }}
+          alignItems="center"
+          justifyContent="center"
+        >
           <Box
-            w={{base:'90%',md:'100%',lg:'90%',xl:'1150px'}}
+            w={{ base: "90%", md: "100%", lg: "90%", xl: "1150px" }}
             display="flex"
-            flexDirection={{base:'column',md:'row',lg:'row',xl:'row'}}
+            flexDirection={{ base: "column", md: "row" }}
             alignItems="center"
             justifyContent="center"
-            gap={{base:'30px',md:'2%',lg:'5%',xl:'100px'}}
+            gap={{ base: "30px", md: "50px", lg: "50px", xl: "100px" }}
           >
             <Box
-              position="relative"
-              w={{base:'300px',md:'300px',lg:'350px',xl:'450px'}}
-              h={{base:'300px',md:'300px',lg:'350px',xl:'450px'}}
-              borderRadius="50%"
+              w={{ base: "250px", md: "300px", lg: "400px", xl: "450px" }}
+              h={{ base: "250px", md: "300px", lg: "400px", xl: "450px" }}
               border="2px solid gray"
+              borderRadius="50%"
+              position="relative"
               display="flex"
               alignItems="center"
               justifyContent="center"
@@ -100,8 +174,8 @@ const SecretofSuccessSection4 = () => {
                   <Box
                     key="index"
                     position="absolute"
-                    w={{base:'60px',md:'80px',lg:'100px',xl:'120px'}}
-                    h={{base:'60px',md:'80px',lg:'100px',xl:'120px'}}
+                    w={{ base: "60px", md: "80px", lg: "100px", xl: "120px" }}
+                    h={{ base: "60px", md: "80px", lg: "100px", xl: "120px" }}
                     bg={theme.colors.col.secondary}
                     display="flex"
                     flexDirection="column"
@@ -109,7 +183,12 @@ const SecretofSuccessSection4 = () => {
                     justifyContent="center"
                     color="white"
                     borderRadius="50%"
-                    fontSize={{base:'0.5rem',md:'0.8rem',lg:'0.9rem',xl:'0.9rem'}}
+                    fontSize={{
+                      base: "0.5rem",
+                      md: "0.8rem",
+                      lg: "0.9rem",
+                      xl: "0.9rem",
+                    }}
                     textAlign="center"
                     ref={(el) => (Animatedcircle.current[index] = el)}
                     _hover={{
@@ -117,16 +196,20 @@ const SecretofSuccessSection4 = () => {
                       color: theme.colors.col.secondary,
                     }}
                     onMouseEnter={() => {
-                      handleMouseEnter(path.text,path.index);
+                      handleMouseEnter(path.text, path.index);
                     }}
                   >
                     <text>{path.name}</text>
                   </Box>
                 );
               })}
-
               <Box
-                fontSize={{base:'0.4rem',md:'0.7rem',lg:'0.8rem',xl:'0.8rem'}}
+                fontSize={{
+                  base: "0.4rem",
+                  md: "0.7rem",
+                  lg: "0.8rem",
+                  xl: "0.8rem",
+                }}
                 color={theme.colors.col.secondary}
                 p="20px"
                 textAlign="center"
@@ -135,38 +218,43 @@ const SecretofSuccessSection4 = () => {
               </Box>
             </Box>
 
-
             <Box
-              w={{base:'100%',md:'350px',lg:'450px',xl:'600px'}}
-              h={{base:'450px',md:'300px',lg:'350px',xl:'450px'}}
+              w={{ base: "98%", md: "350px", lg: "500px", xl: "600px" }}
+              h={{ base: "450px", md: "300px", lg: "400px", xl: "450px" }}
               display="flex"
               flexDirection="column"
               alignItems="center"
-              justifyContent="center"
-              gap={{base:'20px',md:'10px',lg:'15px',xl:'40px'}}
-              textAlign="justify"
+              justifyContent="flex-start"
+              gap='20px'
+              textAlign='justify'
             >
               <Box
                 display="flex"
                 justifyContent="space-between"
-                alignItems={{base:'center',md:'none'}}
-                gap='20px'
-                h={{base:'100px',md:'65px',lg:'75px',xl:'100px'}}
+                gap="20px"
               >
                 <Box
                   bg={theme.colors.col.text}
-                  w={{base:'80px',md:'50px',lg:'60px',xl:'80px'}}
-                  h={{base:'80px',md:'50px',lg:'60px',xl:'80px'}}
+                  w={{ base: "60px", md: "50px", lg: "60px", xl: "80px" }}
+                  h={{ base: "60px", md: "50px", lg: "60px", xl: "80px" }}
                   borderRadius="50%"
                   display="flex"
                   alignItems="center"
                   justifyContent="center"
-                  fontSize={{base:'2rem',md:'1rem',lg:'1.5rem',xl:'2rem'}}
+                  fontSize={{
+                    base: "2rem",
+                    md: "1rem",
+                    lg: "1.5rem",
+                    xl: "2rem",
+                  }}
                   color="white"
                 >
                   <MdOutlineSchedule />
                 </Box>
-                <Box w={{base:'200px',md:'280px',lg:'370px',xl:'450px'}} >
+
+                <Box
+                  w={{ base: "200px", md: "280px", lg: "420px", xl: "450px" }}
+                >
                   {" "}
                   <Box fontSize="1.1rem" mb="10px" fontWeight="600">
                     Chanting
@@ -181,31 +269,37 @@ const SecretofSuccessSection4 = () => {
               <Box
                 display="flex"
                 justifyContent="space-between"
-                alignItems={{base:'center',md:'none'}}
                 gap="20px"
-                h={{base:'100px',md:'65px',lg:'75px',xl:'100px'}}
               >
                 <Box
                   bg={theme.colors.col.text}
-                  w={{base:'80px',md:'50px',lg:'60px',xl:'80px'}}
-                  h={{base:'80px',md:'50px',lg:'60px',xl:'80px'}}
+                  w={{ base: "60px", md: "50px", lg: "60px", xl: "80px" }}
+                  h={{ base: "60px", md: "50px", lg: "60px", xl: "80px" }}
                   borderRadius="50%"
                   display="flex"
                   alignItems="center"
                   justifyContent="center"
-                  fontSize={{base:'2rem',md:'1rem',lg:'1.5rem',xl:'2rem'}}
+                  fontSize={{
+                    base: "2rem",
+                    md: "1rem",
+                    lg: "1.5rem",
+                    xl: "2rem",
+                  }}
                   color="white"
                 >
                   <GoGoal />
                 </Box>
-                <Box w={{base:'200px',md:'280px',lg:'370px',xl:'450px'}}>
+
+                <Box
+                  w={{ base: "200px", md: "280px", lg: "420px", xl: "450px" }}
+                >
                   {" "}
                   <Box fontSize="1.1rem" mb="10px" fontWeight="600">
-                    Aligning Goals
+                    Aligining Goals
                   </Box>
                   <Box fontSize="0.8rem">
-                    Align goals with spiritual values.Break larger goals into
-                    smaller tasks.
+                  Align goals with spiritual values.Break larger goals into
+                  smaller tasks.
                   </Box>
                 </Box>
               </Box>
@@ -213,30 +307,36 @@ const SecretofSuccessSection4 = () => {
               <Box
                 display="flex"
                 justifyContent="space-between"
-                alignItems={{base:'center',md:'none'}}
                 gap="20px"
-                h={{base:'100px',md:'65px',lg:'75px',xl:'100px'}}
               >
                 <Box
                   bg={theme.colors.col.text}
-                  w={{base:'80px',md:'50px',lg:'60px',xl:'80px'}}
-                  h={{base:'80px',md:'50px',lg:'60px',xl:'80px'}}
+                  w={{ base: "60px", md: "50px", lg: "60px", xl: "80px" }}
+                  h={{ base: "60px", md: "50px", lg: "60px", xl: "80px" }}
                   borderRadius="50%"
                   display="flex"
                   alignItems="center"
                   justifyContent="center"
-                  fontSize={{base:'2rem',md:'1rem',lg:'1.5rem',xl:'2rem'}}
+                  fontSize={{
+                    base: "2rem",
+                    md: "1rem",
+                    lg: "1.5rem",
+                    xl: "2rem",
+                  }}
                   color="white"
                 >
                   <GiAwareness />
                 </Box>
-                <Box w={{base:'200px',md:'280px',lg:'370px',xl:'450px'}}>
+
+                <Box
+                  w={{ base: "200px", md: "280px", lg: "420px", xl: "450px" }}
+                >
                   {" "}
                   <Box fontSize="1.1rem" mb="10px" fontWeight="600">
                     Awareness
                   </Box>
                   <Box fontSize="0.8rem">
-                    Focus on the present moment.Perform actions with full
+                  Focus on the present moment.Perform actions with full
                     awareness.
                   </Box>
                 </Box>
@@ -245,34 +345,41 @@ const SecretofSuccessSection4 = () => {
               <Box
                 display="flex"
                 justifyContent="space-between"
-                alignItems={{base:'center',md:'none'}}
                 gap="20px"
-                h={{base:'100px',md:'65px',lg:'75px',xl:'100px'}}
               >
                 <Box
                   bg={theme.colors.col.text}
-                  w={{base:'80px',md:'50px',lg:'60px',xl:'80px'}}
-                  h={{base:'80px',md:'50px',lg:'60px',xl:'80px'}}
+                  w={{ base: "60px", md: "50px", lg: "60px", xl: "80px" }}
+                  h={{ base: "60px", md: "50px", lg: "60px", xl: "80px" }}
                   borderRadius="50%"
                   display="flex"
                   alignItems="center"
                   justifyContent="center"
-                  fontSize={{base:'2rem',md:'1rem',lg:'1.5rem',xl:'2rem'}}
+                  fontSize={{
+                    base: "2rem",
+                    md: "1rem",
+                    lg: "1.5rem",
+                    xl: "2rem",
+                  }}
                   color="white"
                 >
                   <FaScaleUnbalancedFlip />
                 </Box>
-                <Box w={{base:'200px',md:'280px',lg:'370px',xl:'450px'}} >
+
+                <Box
+                  w={{ base: "200px", md: "280px", lg: "420px", xl: "450px" }}
+                >
                   {" "}
                   <Box fontSize="1.1rem" mb="10px" fontWeight="600">
                     Balanced Life
                   </Box>
                   <Box fontSize="0.8rem">
-                    Balance material and spiritual responsibilities.Take care of
+                  Balance material and spiritual responsibilities.Take care of
                     your physical health.
                   </Box>
                 </Box>
               </Box>
+
             </Box>
           </Box>
         </Box>
